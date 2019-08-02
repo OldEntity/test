445d8f7409f8f9d40a535792f053e52cfcb5f808
LearnLi <wlf2034002539@gmail.com>
2019-08-02 10:54:05 +0800

    更新shiro

---
+++ test-shiro/src/main/java/com/lishuo/testshiro/TestShiroApplication.java
@@ -0,0 +1,13 @@
+package com.lishuo.testshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestShiroApplication.class, args);
+    }
+
+}
